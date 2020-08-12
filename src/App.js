f49a3d85bf8f54e79d20ed097ce0876a1dc61b5a
onewoorks <irwanbibrahim@onewoorks.com>
2020-08-13 03:36:38 +0800

    set enviroment

--- src/App.js
+++ src/App.js
@@ -9,43 +9,52 @@ import Welcome from './components/welcome'
 
 function App() {
     const [auth, setAuth] = React.useState({})
-    console.log(process.env)
+
     React.useEffect(() => {
-        const keycloak = Keycloak('./keycloak.json')
+        const keycloak = Keycloak({
+            realm: process.env.REACT_APP_SSO_REALM ,
+            'url': 'https://sso.pengurusanemas.my/auth',
+            'ssl-required': 'external',
+            clientId: 'apps_central',
+            'public-client': true,
+            'confidential-port': 0,
+        })
         keycloak
             .init({
                 onLoad: 'login-required',
                 promiseType: 'native',
-                checkLoginIframe: false
+                checkLoginIframe: false,
             })
-            .then(authenticated => {
+            .then((authenticated) => {
                 setAuth({
-                  keycloak: keycloak,
-                  tokenParsed: keycloak.idTokenParsed,
-                  authenticated: authenticated,
-                  apps: keycloak.resourcesAccess
+                    keycloak: keycloak,
+                    tokenParsed: keycloak.idTokenParsed,
+                    authenticated: authenticated,
+                    apps: keycloak.resourcesAccess,
                 })
             })
-    },[])
+    }, [])
 
-    if(auth.keycloak){
-      if(auth.authenticated){
-        return (
-        <div className="container-fluid">
-            <div style={{ marginTop: 20 }}>
-                <Welcome auth={auth} />
-                <PageMain logout={auth.keycloak} allowed_apps={auth.tokenParsed.apps_allowed} />
-            </div>
-            <PageFooter />
-        </div>
-    )
-      } else {
-        return <div>unable to authenticate!</div>
-      } 
+    if (auth.keycloak) {
+        if (auth.authenticated) {
+            return (
+                <div className="container-fluid">
+                    <div style={{ marginTop: 20 }}>
+                        <Welcome auth={auth} />
+                        <PageMain
+                            logout={auth.keycloak}
+                            allowed_apps={auth.tokenParsed.apps_allowed}
+                        />
+                    </div>
+                    <PageFooter />
+                </div>
+            )
+        } else {
+            return <div>unable to authenticate!</div>
+        }
     }
 
     return <div>Initializing authenthication</div>
-    
 }
 
 export default App
